984c3386936093a8631c87914f970343f92dc54d
Konrad Mrozek <konrad.mrozek@gmail.com>
2009-07-01 09:55:01 +0200

    Add documentation to functions in rgb.clj.

--- rgb.clj
+++ rgb.clj
@@ -1,5 +1,8 @@
-(defn hex [x] (. Integer toHexString x))
+(defn hex [x] 
+	"Returns hex representation of int"
+	(. Integer toHexString x))
 
 (defn rgb [r g b]
+	"Returns hex of RGB color code"
   (hex 
    (+ b (* 256 (+ g (* 256 r))))))
