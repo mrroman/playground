715780aa87593a688a4f63d985a14a01a135f18c
Konrad Mrozek <konrad.mrozek@gmail.com>
2009-11-12 15:16:18 +0100

    Added hello.clj to repository

--- hello.clj
+++ hello.clj
@@ -1,2 +1,8 @@
 (defn hello [] 
   (println "Hello world!"))
+
+(defn petla [x y]
+  (loop [i 0 j 0]
+    (when (< j y)
+      (print (str "(" i "," j") "))
+      (recur (mod (inc i) x) (if (= i (dec x)) (inc j) j))))))
