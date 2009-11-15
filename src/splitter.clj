1f5eb193ab4e565046e4154101cfdd603d69cbfe
Konrad Mrożek <konrad@thinkpad.(none)>
2009-11-15 22:13:12 +0100

    Modifications in splitter. One more version.

--- src/splitter.clj
+++ src/splitter.clj
@@ -1,3 +1,13 @@
+(def test-string "11&&22&&33&aa&&bb&c&dd")
+
+(defn splitter [x]
+  (let [s (.split x "&")]
+    (loop [data s result [] current []]
+      (when (not (empty? s))
+	(if (= (first data) "")
+	  (recur (rest data) result current)
+	  (recur (rest data) 
+
 (defn split-to-objects [text]
   (let [x (.split text "&")]
     (split-with #(= "" %) x)))
@@ -7,9 +17,16 @@
     (if (empty? x)
       (cons z y)
       (if (= (first x) d)
-	(recur [(rest x) (cons z y) []])
-	(recur [(rest x) y (cons (first x) z)])
-	)0
+	(recur (rest (rest x)) y (cons (first (rest x)) z))
+	(recur (rest x) (cons z y) [])
+	)
       )
     )
   )
+
+(defn myreduce [x y]
+  (if (= y "")
+    (cons y x)
+    (if (= (first x) "")
+      (cons y (rest x))
+      (cons y (list x)))))
